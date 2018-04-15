030b3b741d182b392aed4fa3cb5be4113651379f
Auston Stewart <auston@shedbuilt.net>
2018-04-15 09:37:45 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-case "$SHED_BUILDMODE" in
+case "$SHED_BUILD_MODE" in
     toolchain)
         sed -i s/mawk// configure
         ./configure --prefix=/tools \
@@ -20,22 +20,22 @@ case "$SHED_BUILDMODE" in
                     --enable-widec || return 1
         ;;
 esac
-make -j $SHED_NUMJOBS || return 1
-make DESTDIR="$SHED_FAKEROOT" install || return 1
+make -j $SHED_NUM_JOBS || return 1
+make DESTDIR="$SHED_FAKE_ROOT" install || return 1
 
-if [ "$SHED_BUILDMODE" != 'toolchain' ]; then
-    mkdir -v "${SHED_FAKEROOT}/lib"
-    mv -v ${SHED_FAKEROOT}/usr/lib/libncursesw.so.6* ${SHED_FAKEROOT}/lib
-    ln -sfv ../../lib/$(readlink ${SHED_FAKEROOT}/usr/lib/libncursesw.so) ${SHED_FAKEROOT}/usr/lib/libncursesw.so
+if [ "$SHED_BUILD_MODE" != 'toolchain' ]; then
+    mkdir -v "${SHED_FAKE_ROOT}/lib"
+    mv -v ${SHED_FAKE_ROOT}/usr/lib/libncursesw.so.6* ${SHED_FAKE_ROOT}/lib
+    ln -sfv ../../lib/$(readlink ${SHED_FAKE_ROOT}/usr/lib/libncursesw.so) ${SHED_FAKE_ROOT}/usr/lib/libncursesw.so
     for lib in ncurses form panel menu ; do
-        rm -vf                    ${SHED_FAKEROOT}/usr/lib/lib${lib}.so
-        echo "INPUT(-l${lib}w)" > ${SHED_FAKEROOT}/usr/lib/lib${lib}.so
-        ln -sfv ${lib}w.pc        ${SHED_FAKEROOT}/usr/lib/pkgconfig/${lib}.pc
+        rm -vf                    ${SHED_FAKE_ROOT}/usr/lib/lib${lib}.so
+        echo "INPUT(-l${lib}w)" > ${SHED_FAKE_ROOT}/usr/lib/lib${lib}.so
+        ln -sfv ${lib}w.pc        ${SHED_FAKE_ROOT}/usr/lib/pkgconfig/${lib}.pc
     done
-    rm -vf                     ${SHED_FAKEROOT}/usr/lib/libcursesw.so
-    echo "INPUT(-lncursesw)" > ${SHED_FAKEROOT}/usr/lib/libcursesw.so
-    ln -sfv libncurses.so      ${SHED_FAKEROOT}/usr/lib/libcurses.so
+    rm -vf                     ${SHED_FAKE_ROOT}/usr/lib/libcursesw.so
+    echo "INPUT(-lncursesw)" > ${SHED_FAKE_ROOT}/usr/lib/libcursesw.so
+    ln -sfv libncurses.so      ${SHED_FAKE_ROOT}/usr/lib/libcurses.so
     # Install documentation
-    mkdir -pv ${SHED_FAKEROOT}/usr/share/doc/ncurses-6.1
-    cp -v -R doc/* ${SHED_FAKEROOT}/usr/share/doc/ncurses-6.1
+    mkdir -pv ${SHED_FAKE_ROOT}/usr/share/doc/ncurses-6.1
+    cp -v -R doc/* ${SHED_FAKE_ROOT}/usr/share/doc/ncurses-6.1
 fi
